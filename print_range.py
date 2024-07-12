0098b4d2349ebf70d5cbc8b5b0bc7b0c9b760dd3
Khemkaran Sevta <khemkaransevta@gmail.com>
2024-07-13 00:29:36 +0530

    add sum function is added

---
+++ print_range.py
@@ -0,0 +1,11 @@
+def print_range(n):
+    for i in range(n):
+        print(i, end=" ")
+
+def main():
+    print_range(10)
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ sum_values.py
@@ -0,0 +1,8 @@
+def add_val(a : int,b : int) -> int:
+    print(f"Sum of a and b is {a+b}")
+    return a+b
+
+if __name__ == "__main__":
+    add_val(500,60)
+
+    
